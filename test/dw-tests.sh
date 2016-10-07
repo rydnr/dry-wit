4b6da4d59689abb5839f6b563e74fee6aed170a2
Jose <chous@acm-sl.org>
2016-10-07 08:12:40 +0200

    Started working on a unit test framework for dry-wit.

---
+++ test/dw-tests.sh
@@ -0,0 +1,35 @@
+#!/bin/bash dry-wit-test
+# Copyright 2016-today Automated Computing Machinery S.L.
+# Distributed under the terms of the GNU General Public License v3
+
+# set -o xtrace
+
+function usage() {
+cat <<EOF
+$SCRIPT_NAME
+$SCRIPT_NAME [-h|--help]
+(c) 2016-today ACM-SL
+    Distributed this under the GNU General Public License v3.
+
+Runs all tests implemented for dry-wit itself.
+
+Common flags:
+    * -h | --help: Display this message.
+    * -v: Increase the verbosity.
+    * -vv: Increase the verbosity further.
+    * -q | --quiet: Be silent.
+EOF
+}
+
+## Defines the errors.
+## dry-wit hook
+function defineErrors() {
+  addError "PARSECOMMONINPUT_DOES_NOT_SUPPORT_SMALL_H" "The _parseCommonInput method does not support -h";
+}
+
+## Tests for dry-wit's _parseCommonInput.
+function _parseCommonInput_test() {
+
+  _parseCommonInput "-h";
+  assertTrue [ 1 == 1 ], CHECK_INPUT_DOES_NOT_SUPPORT_SMALL_H;
+}
