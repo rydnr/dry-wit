a2b4e89f27e47a26f4bd54b5728595b8262f313e
Jose <chous@acm-sl.org>
2016-10-08 11:48:54 +0200

    Implemented some more tests on dry-wit

--- test/dw-tests.sh
+++ test/dw-tests.sh
@@ -24,7 +24,11 @@ EOF
 ## Defines the errors.
 ## dry-wit hook
 function defineErrors() {
-  addError "PARSECOMMONINPUT_DOES_NOT_SUPPORT_SMALL_H" "The _parseCommonInput method does not support -h";
+  addError PARSECOMMONINPUT_DOES_NOT_SUPPORT_SMALL_H "The _parseCommonInput method does not support -h";
+  addError RETRIEVEUIDFROMUSER_CANNOT_RETRIEVE_ROOT_UID "retrieveUidFromUser \"root\" failed";
+  addError RETRIEVEUIDFROMUSER_FAILED_FOR_USER_ROOT "retrieveUidFromUser \"root\" returned an invalid uid";
+  addError RETRIEVEGROUPFROMGID_DOES_NOT_CHECK_INPUT_PARAMETERS "retrieveGroupFromGid does not check input parameters";
+  addError RETRIEVEGROUPFROMGID_FAILED_FOR_GROUP_ROOT "retrieveGroupFromGid returned an invalid gid for group root";
 }
 
 ## Tests for dry-wit's _parseCommonInput.
@@ -34,6 +38,20 @@ function _parseCommonInput_test() {
   assertNoErrorThrown PARSECOMMONINPUT_DOES_NOT_SUPPORT_SMALL_H;
 }
 
-function _other_test() {
-  echo "other test";
+function retrieveUidFromUser_test() {
+  local _expectedUid=$(grep root /etc/passwd | cut -d':' -f 3);
+  retrieveUidFromUser "root";
+  assertTrue $? RETRIEVEUIDFROMUSER_CANNOT_RETRIEVE_ROOT_UID;
+  local _actualUid=${RESULT};
+  assertEquals ${_expectedUid} ${_actualUid} RETRIEVEUIDFROMUSER_FAILED_FOR_USER_ROOT;
+}
+
+function retrieveGroupFromGid_test() {
+  local _gid=$(grep root /etc/group | cut -d':' -f 3);
+  retrieveGroupFromGid;
+  assertErrorThrown RETRIEVEGROUPFROMGID_DOES_NOT_CHECK_INPUT_PARAMETERS;
+  retrieveGroupFromGid "${_gid}";
+  assertTrue $? RETRIEVEGROUPFROMGID_CANNOT_RETRIEVE_ROOT_GID;
+  local _actualGroup=${RESULT};
+  assertEquals "root" ${_actualGroup} RETRIEVEGROUPFROMGID_FAILED_FOR_GROUP_ROOT;
 }
