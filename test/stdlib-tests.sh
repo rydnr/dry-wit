bd4dad31a59919ac61a28446368e46a0f8bcea3a
josesl <jose.sanleandro@osoco.es>
2018-12-11 08:46:33 +0100

    Minor fixes and added some tests on stdlib.dw

---
+++ test/stdlib-tests.sh
@@ -0,0 +1,71 @@
+#!/bin/bash ../src/dry-wit-test
+# Copyright 2016-today Automated Computing Machinery S.L.
+# Distributed under the terms of the GNU General Public License v3
+
+# set -o xtrace
+
+function isZero_test() {
+  isZero 0;
+  Assert.isTrue $? "isZero 0 failed";
+
+  isZero 1;
+  Assert.isFalse $? "isZero 1 failed";
+
+  isZero "0";
+  Assert.isTrue $? "isZero '0' failed";
+
+  isZero "1";
+  Assert.isFalse $? "isZero '1' failed";
+
+  isZero "a";
+  Assert.isFalse $? "isZero 'a' failed";
+}
+
+function isEmpty_test() {
+  isEmpty "";
+  Assert.isTrue $? "isEmpty '' failed";
+
+  isEmpty "a";
+  Assert.isFalse $? "isEmpty 'a' failed";
+
+  isEmpty 0;
+  Assert.isFalse $? "isEmpty 0 failed";
+}
+
+function isTrue_test() {
+  isTrue 0;
+  Assert.isTrue $? "isTrue 0 failed";
+
+  isTrue 1;
+  Assert.isFalse $? "isTrue 1 failed";
+}
+
+function isFalse_test() {
+  isFalse 1;
+  Assert.isTrue $? "isFalse 1 failed";
+
+  isFalse 0;
+  Assert.isFalse $? "isFalse 0 failed";
+}
+
+function areEqual_test() {
+  areEqual "a" "a";
+  Assert.isTrue $? "areEqual 'a' 'a' failed";
+
+  areEqual "a" "b";
+  Assert.isFalse $? "areEqual 'a' 'b' failed";
+}
+
+function areNotEqual_test() {
+  areNotEqual "a" "b";
+  Assert.isTrue $? "areNotEqual 'a' 'b' failed";
+
+  areNotEqual "a" "a";
+  Assert.isFalse $? "areNotEqual 'a' 'a' failed";
+}
+
+function isFunctionDefined_test() {
+  isFunctionDefined "isFunctionDefined_test";
+  Assert.isTrue $? "isFunctionDefined 'isFunctionDefined_test' failed";
+}
+#
