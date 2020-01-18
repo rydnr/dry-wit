0748a855250a9d330c44ad0d64393cffedf42278
Jose San Leandro <josesla@ext.inditex.com>
2020-01-18 10:04:00 +0100

    Some enhancements on dry-wit-test to report the total number of tests

--- test/test-all.sh
+++ test/test-all.sh
@@ -3,9 +3,20 @@
 cp ../src/modules/*.dw ~/.dry-wit/src/modules/ 2> /dev/null;
 cp ../src/dry-wit* ~/.dry-wit/src/ 2> /dev/null;
 
-for _f in *-tests.sh; do
+declare -i TOTAL_TESTS=0;
+declare -i TOTAL_PASSED_TESTS=0;
+declare -i TOTAL_FAILED_TESTS=0;
+
+for _f in $(find . -name '*-tests.sh'); do
   rm -f /tmp/${_f}* 2> /dev/null
   ./${_f};
+  source ./.$(basename ${_f}).env;
+  TOTAL_TESTS=$((TOTAL_TESTS + TESTS_FOUND));
+  TOTAL_PASSED_TESTS=$((TOTAL_PASSED_TESTS + PASSED_TESTS));
+  TOTAL_FAILED_TESTS=$((TOTAL_FAILED_TESTS + FAILED_TESTS));
   cat /tmp/${_f}* 2> /dev/null
 done
+echo "Total tests: ${TOTAL_TESTS}";
+echo "Passed tests: ${TOTAL_PASSED_TESTS}";
+echo "Failed tests: ${TOTAL_FAILED_TESTS}";
 # vim: syntax=sh ts=2 sw=2 sts=4 sr noet
