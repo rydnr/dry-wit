8da5740bcca8df34b5f989de3c1b509e9b02f5dd
Jose San Leandro <josesla@ext.inditex.com>
2020-02-10 08:03:30 +0100

    Minor refactoring in cli.dw

--- test/test-all.sh
+++ test/test-all.sh
@@ -10,8 +10,8 @@ declare -i TOTAL_FAILED_TESTS=0;
 for _f in $(find . -name '*-tests.sh'); do
   rm -f /tmp/${_f}* 2> /dev/null
   ./${_f};
-  if [[ -f "./.$(basename ${_f}).env" ]] then
-     source "./.$(basename ${_f}).env";
+  if [[ -f "./.$(basename ${_f}).env" ]]; then
+    source "./.$(basename ${_f}).env";
   fi
   TOTAL_TESTS=$((TOTAL_TESTS + TESTS_FOUND));
   TOTAL_PASSED_TESTS=$((TOTAL_PASSED_TESTS + PASSED_TESTS));
