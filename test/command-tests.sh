6feec08071e412f2ff7584bc7f8889bfde219b59
Jose San Leandro <josesla@ext.inditex.com>
2020-02-22 11:04:00 +0100

    Added some tests for command.dw

---
+++ test/command-tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash dry-wit
+# Copyright 2020-today Automated Computing Machinery S.L.
+# Distributed under the terms of the GNU General Public License v3
+
+function runCommand_works_test() {
+  runCommand "ls";
+  local _result="${RESULT}";
+  Assert.isTrue $? "runCommand 'ls' failed";
+  Assert.isNotEmpty "${_result}" "runCommand 'ls' was silent";
+}
+
+function runCommandAsUidGid_works_test() {
+  local _uid="$(id -u)";
+  local _gid="$(id -g)";
+  runCommandAsUidGid ${_uid} ${_gid} "${PWD}" "ls";
+  local _result="${RESULT}";
+  Assert.isTrue $? "runCommandAsUidGid 'ls' was silent";
+}
+setScriptDescription "Runs all tests implemented for command.dw";
+# vim: syntax=sh ts=2 sw=2 sts=4 sr noet
