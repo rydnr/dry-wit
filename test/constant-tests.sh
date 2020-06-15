737167e82684aa997c710cf1f137618b05e69f7b
Jose San Leandro <jose.sanleandro@osoco.es>
2020-06-15 11:15:55 +0200

    Fixed some failing tests

---
+++ test/constant-tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash dry-wit
+# Copyright 2020-today Automated Computing Machinery S.L.
+# Distributed under the terms of the GNU General Public License v3
+
+function evalConstant_works_for_predefined_colors_test() {
+  local _expected="\033[0;37m";
+  local _colorName=GRAY;
+  DW.getGlobalVariableName "COLOR" "${_colorName}";
+  local _constant="${RESULT}";
+  evalConstant ${_constant};
+  local -i _rescode=$?;
+  local _actual="${RESULT}";
+  Assert.isTrue ${_rescode} "evalConstant ${_constant} returned false";
+  Assert.areEqual "${_expected}" "${_actual}" "evalConstant ${_constant} failed";
+}
+
+setScriptDescription "Runs all tests implemented for constant.dw";
+# vim: syntax=sh ts=2 sw=2 sts=4 sr noet
