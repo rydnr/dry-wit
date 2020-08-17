613806c2fe4109c7ce67b8bf91b25415db4b267d
Jose San Leandro <jose.sanleandro@osoco.es>
2020-08-17 18:40:36 +0200

    Supporting the case of removing text using sed

--- test/ext/sed-tests.sh
+++ test/ext/sed-tests.sh
@@ -13,6 +13,14 @@ function safeSedReplacement_works_test() {
   Assert.areEqual "'foo'='${_text}'" "${_result}" "echo \"'foo'='|foo|'\" | safeSedReplacement \"|foo|\" \"${_text}\" failed";
 }
 
+function safeSedReplacement_works_for_removing_text_test() {
+  local _result;
+  _result="$(echo "'foo'='|foo|'" | safeSedReplacement "|foo|" "")";
+  local _rescode=$?;
+  Assert.isTrue ${_rescode} "echo \"'foo'='|foo|'\" | safeSedReplacement \"|foo|\" \"\" failed";
+  Assert.areEqual "'foo'=''" "${_result}" "echo \"'foo'='|foo|'\" | safeSedReplacement \"|foo|\" \"\" failed";
+}
+
 function safeMinusIReplacement_works_test() {
   local _text='$m^\\y/t(e)x+t[ ]';
   local _result;
@@ -24,6 +32,18 @@ function safeMinusIReplacement_works_test() {
   local _rescode=$?;
   Assert.isTrue ${_rescode} "safeSedReplacement \"|foo|\" \"${_text}\" [file] failed";
   Assert.areEqual "'foo'='${_text}'" "${_result}" "safeSedReplacement \"|foo|\" \"${_text}\" [file] failed";
+}
+
+function safeMinusIReplacement_works_for_removing_text_test() {
+  local _result;
+  createTempFile;
+  local _file="${RESULT}";
+  echo "'foo'='|foo|'" > "${_file}";
+  safeSedMinusIReplacement "|foo|" "" "${_file}";
+  _result="$(cat "${_file}")";
+  local _rescode=$?;
+  Assert.isTrue ${_rescode} "safeSedReplacement \"|foo|\" \"\" [file] failed";
+  Assert.areEqual "'foo'=''" "${_result}" "safeSedReplacement \"|foo|\" \"\" [file] failed";
 }
 
 setScriptDescription "Runs all tests implemented for sed.dw";
