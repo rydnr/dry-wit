17b4084e4220b49ad0833339805eb07bde7f73e1
Jose San Leandro <jose.sanleandro@osoco.es>
2020-10-06 13:14:18 +0200

    Removed bug with duplicated envvars

--- test/envvar-tests.sh
+++ test/envvar-tests.sh
@@ -36,14 +36,63 @@ function debugAssociativeArray_prints_each_entry_in_the_debug_file_test() {
 }
 
 function defineEnvVar_adds_an_environment_variable_test() {
-  defineEnvVar "MY_VAR" MANDATORY "My env var" "foo" "date";
+  defineEnvVar MY_VAR MANDATORY "My env var" "foo" "date";
   ENVVAR.getEnvironmentVariablesVariableName;
   local _variableName=${RESULT};
   local -n _envVariables=${_variableName};
   Assert.isNotEmpty "${_envVariables[@]}" "Environment variables array is empty";
   # The next line is critical!
   local _array="${_envVariables[@]}";
-  Assert.arrayContains "MY_VAR" "${_array}" 'Environment variables array does not contain MY_VAR.';
+  Assert.arrayContains MY_VAR "${_array}" 'Environment variables array does not contain MY_VAR.';
+}
+
+function defineEnvVar_does_not_add_an_already_existing_environment_variable_to_envVars_names_test() {
+  local _expected="first value";
+  defineEnvVar MY_VAR_1 MANDATORY "My env var" "${_expected}";
+  Assert.areEqual "${MY_VAR_1}" "${_expected}" "Invalid value of MY_VAR_1";
+  defineEnvVar MY_VAR_1 MANDATORY "My env var" "other value";
+  Assert.isFalse $? "MY_VAR_1 shouldn't be defined more than once";
+  Assert.areEqual "${MY_VAR_1}" "${_expected}" "MY_VAR_1 should remain constant when using defineEnvVar";
+  ENVVAR.getEnvironmentVariablesVariableName;
+  local _variableName=${RESULT};
+  local -n _envVariables=${_variableName};
+  local _oldIFS="${IFS}";
+  local _envVar;
+  local -i _matches=0;
+  IFS="${DWIFS}";
+  for _envVar in ${_envVariables[@]}; do
+    if areEqual "${_envVar}" MY_VAR_1; then
+      _matches=$((_matches+1));
+    fi
+  done
+  isGreaterThan ${_matches} 1;
+  Assert.isFalse $? "MY_VAR_1 is annotated ${_matches} times, instead of 1";
+}
+
+function defineEnvVar_does_not_add_an_already_existing_environment_variable_test() {
+  local _expected="first value";
+  defineEnvVar MY_VAR_2 MANDATORY "My env var" "${_expected}";
+  Assert.isTrue $? "Couldn't define MY_VAR_2";
+  Assert.areEqual "${MY_VAR_2}" "${_expected}" "Invalid value of MY_VAR_2";
+  defineEnvVar MY_VAR_2 MANDATORY "My env var" "other value";
+  Assert.isFalse $? "MY_VAR_2 shouldn't be defined more than once";
+}
+
+function defineEnvVar_overrides_an_existing_environment_variable_test() {
+  local _expected1="first value";
+  defineEnvVar MY_VAR_3 MANDATORY "My env var" "${_expected}";
+  Assert.areEqual "${MY_VAR_3}" "${_expected}" "Invalid value of MY_VAR_3";
+  local _expected2="other value";
+  defineEnvVar MY_VAR_3 MANDATORY "My env var" "${_expected2}";
+  Assert.areEqual "${MY_VAR_3}" "${_expected2}" "MY_VAR_3 should have changed";
+}
+
+function isEnvVarDefined_detects_existing_envvars_test() {
+  defineEnvVar MY_VAR_4 MANDATORY "My env var" "whatever";
+  isEnvVarDefined MY_VAR_4;
+  Assert.isTrue $? "MY_VAR_4 is not detected";
+  isEnvVarDefined "MY_VAR_4_$$";
+  Assert.isFalse $? "MY_VAR_4_$$ is detected and it shouldn't";
 }
 
 function empty_vars_are_not_included_as_environment_variables_test() {
