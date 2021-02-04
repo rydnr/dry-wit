f3c55502b384163f0e9a34856bf11f1d59554620
Jose San Leandro <jose.sanleandro@osoco.es>
2021-02-04 10:14:50 +0100

    Fixed logging.dw tests

--- test/logging-tests.sh
+++ test/logging-tests.sh
@@ -26,28 +26,34 @@ function empty_log_category() {
 }
 
 function LOGGING.alignRightForWidth_finds_the_correct_offset_test() {
-  local -i _width=212;
-  local _message="[2020/06/11 18:28:52<logging-tests|myCategory>.] LOGGING.buildLogCategoryPrefix works";
-  local _outcome="pass";
-  LOGGING.alignRightForWidth "${_message}" "${_outcome}" ${_width};
+  local -i _termWidth=212;
+  local _logMessage="[2020/06/11 18:28:52<logging-tests|myCategory>.] LOGGING.buildLogCategoryPrefix works";
+  local _outcome="did not pass";
+  LOGGING.processLogOutcomeTokens "${_outcome}";
+  LOGGING.logOutcomeTokensLength;
+  local -i _logOutcomeTokensLength=${RESULT};
+  LOGGING.alignRightForWidth "${_logMessage}" ${_termWidth};
   local -i _rescode=$?;
-  local -i _offset=${RESULT};
+  local -i _actual=${RESULT};
   Assert.isTrue ${_rescode} "LOGGING.alignRightForWidth returned FALSE";
-  Assert.areEqual $((${_width}-${#_message}-${#_outcome}-2-1)) ${_offset} "LOGGING.alignRightForWidth doesn't find the correct offset";
+  local -i _expected=$((${_termWidth}-(${#_logMessage}+${_logOutcomeTokensLength})%${_termWidth} + 1));
+  Assert.areEqual ${_expected} ${_actual} "LOGGING.alignRightForWidth doesn't find the correct offset in nested logging";
 }
 
 function LOGGING.alignRightForWidth_finds_the_correct_offset_in_nested_logging_test() {
-  local -i _width=212;
+  local -i _termWidth=212;
   local _message="This is a INFO message";
   local _logMessage="[2020/06/11 18:43:14<logging-tests|main:aeourch:3138465:.apurchoaekj/olekh:main:the_log_category_is_propagated_to_nested_logging_test>.] ${_message}";
   local _outcome="....";
-  logInfo -n "${_message}";
-  LOGGING.alignRightForWidth "${_message}" "${_outcome}" ${_width};
+  LOGGING.processLogOutcomeTokens "${_outcome}";
+  LOGGING.logOutcomeTokensLength;
+  local -i _logOutcomeTokensLength=${RESULT};
+  LOGGING.alignRightForWidth "${_logMessage}" ${_termWidth};
   local -i _rescode=$?;
-  local -i _offset=${RESULT};
-  logResult "done";
+  local -i _actual=${RESULT};
   Assert.isTrue ${_rescode} "LOGGING.alignRightForWidth returned FALSE";
-  Assert.areEqual $((${_width}-${#_logMessage}-${#_outcome}-2-1)) ${_offset} "LOGGING.alignRightForWidth doesn't find the correct offset in nested logging";
+  local -i _expected=$((${_termWidth}-(${#_logMessage}+${_logOutcomeTokensLength})%${_termWidth} + 1));
+  Assert.areEqual ${_expected} ${_actual} "LOGGING.alignRightForWidth doesn't find the correct offset in nested logging";
 }
 
 setScriptDescription "Runs all tests implemented for logging.dw";
