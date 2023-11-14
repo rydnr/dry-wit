e385710f851a9b573288b9c2cb19a33f0d31a7fc
rydnr <github@acm-sl.org>
2023-11-14 10:05:17 +0100

    Updated version

--- nix/flake.nix
+++ nix/flake.nix
@@ -35,7 +35,7 @@
         org = "rydnr";
         repo = "dry-wit";
         pname = "${org}-${repo}";
-        version = "3.0.2";
+        version = "3.0.4";
         pkgs = import nixos { inherit system; };
         description = "Dry-wit bash framework";
         license = pkgs.lib.licenses.gpl3;
