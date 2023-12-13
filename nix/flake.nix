5b55b139c445110ac29ff2bd0a1a6517069bbf46
rydnr <github@acm-sl.org>
2023-12-13 21:26:44 +0100

    Separated dry-wit-test from dry-wit
    Fixed return code of whole scripts
    
    Changes in cli.dw, nix-flake.dw and git.dw

--- nix/flake.nix
+++ nix/flake.nix
@@ -35,7 +35,7 @@
         org = "rydnr";
         repo = "dry-wit";
         pname = "${org}-${repo}";
-        version = "3.0.6";
+        version = "3.0.7";
         pkgs = import nixos { inherit system; };
         description = "Dry-wit bash framework";
         license = pkgs.lib.licenses.gpl3;
@@ -51,11 +51,29 @@
             installPhase = ''
               mkdir -p $out
               cp -r src/* $out
+              rm -f src/dry-wit-test
               cp README.md LICENSE $out/
               substituteInPlace $out/dry-wit \
                 --replace "#!/usr/bin/env bash" "#!/usr/bin/env ${sh}/bin/${sh-name}"
+            '';
+
+            meta = with pkgs.lib; {
+              inherit description homepage license maintainers;
+            };
+          };
+        dry-wit-test-for = { dry-wit, sh, sh-name }:
+          pkgs.stdenv.mkDerivation rec {
+            inherit pname version;
+            src = ../.;
+            buildInputs = [ sh ];
+            phases = [ "unpackPhase" "installPhase" ];
+
+            installPhase = ''
+              mkdir -p $out
+              cp -r src/dry-wit-test $out
+              cp README.md LICENSE $out/
               substituteInPlace $out/dry-wit-test \
-                --replace "#!/usr/bin/env dry-wit" "#!/usr/bin/env ${out}/bin/dry-wit"
+                --replace "#!/usr/bin/env dry-wit" "#!/usr/bin/env ${dry-wit}/bin/dry-wit"
             '';
 
             meta = with pkgs.lib; {
@@ -76,6 +94,22 @@
             sh-name = "zsh";
           };
           dry-wit-fish = dry-wit-for {
+            sh = pkgs.fish;
+            sh-name = "fish";
+          };
+          dry-wit-test-default = dry-wit-test-bash5;
+          dry-wit-test-bash5 = dry-wit-test-for {
+            dry-wit = dry-wit-bash5;
+            sh = pkgs.bash_5;
+            sh-name = "bash";
+          };
+          dry-wit-test-zsh = dry-wit-for {
+            dry-wit = dry-wit-zsh;
+            sh = pkgs.zsh;
+            sh-name = "zsh";
+          };
+          dry-wit-test-fish = dry-wit-for {
+            dry-wit = dry-wit-fish;
             sh = pkgs.fish;
             sh-name = "fish";
           };
