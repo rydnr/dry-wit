9400050f9b9167f1f05f93801c00218c00aedcf9
rydnr <github@acm-sl.org>
2024-11-17 07:59:56 +0100

    Fixed a bug and implemented extractParameterFromUrl

--- test/url-tests.sh
+++ test/url-tests.sh
@@ -5,38 +5,49 @@
 # set -o xtrace
 
 function urlEncode_works_test() {
-  DW.import url;
-  local _expectedEncodedUrl="https%3A%2F%2Fwww.example.com";
-  local _targetUrl='https://www.example.com';
-  local _actualEncodedUrl="$(urlEncode "${_targetUrl}")";
-  local -i _rescode=$?;
-  Assert.isTrue ${_rescode} "urlEncode \"${_targetUrl}\" failed";
-  Assert.areEqual "${_expectedEncodedUrl}" "${_actualEncodedUrl}" "urlEncode \"${_targetUrl}\" returned an invalid value (${_actualEncodedValue})";
+  DW.import url
+  local _expectedEncodedUrl="https%3A%2F%2Fwww.example.com"
+  local _targetUrl='https://www.example.com'
+  local _actualEncodedUrl="$(urlEncode "${_targetUrl}")"
+  local -i _rescode=$?
+  Assert.isTrue ${_rescode} "urlEncode \"${_targetUrl}\" failed"
+  Assert.areEqual "${_expectedEncodedUrl}" "${_actualEncodedUrl}" "urlEncode \"${_targetUrl}\" returned an invalid value (${_actualEncodedValue})"
 }
 
 function extractPathSegmentFromUrl_works_test() {
-  DW.import url;
-  local _expectedPathSegment="segment0";
-  local _targetUrl='https://www.example.com/segment0/segment1/segment2/lastSegment?queryParamName0=queryParamValue0&queryParamName1=queryParamValue1;part';
-  extractPathSegmentFromUrl "${_targetUrl}" 0;
-  local -i _rescode=$?;
-  local _actualPathSegment="${RESULT}";
-  Assert.isTrue ${_rescode} "extractPathSegmentFromUrl \"${_targetUrl}\" 0 failed";
-  Assert.areEqual "${_expectedPathSegment}" "${_actualPathSegment}" "extractPathSegmentFromUrl \"${_targetUrl}\" 0 returned an invalid value (${_actualPathSegment})";
-  _expectedPathSegment="segment1";
-  extractPathSegmentFromUrl "${_targetUrl}" 1;
-  _rescode=$?;
-  _actualPathSegment="${RESULT}";
-  Assert.isTrue ${_rescode} "extractPathSegmentFromUrl \"${_targetUrl}\" 1 failed";
-  Assert.areEqual "${_expectedPathSegment}" "${_actualPathSegment}" "extractPathSegmentFromUrl \"${_targetUrl}\" 1 returned an invalid value (${_actualPathSegment})";
+  DW.import url
+  local _expectedPathSegment="segment0"
+  local _targetUrl='https://www.example.com/segment0/segment1/segment2/lastSegment?queryParamName0=queryParamValue0&queryParamName1=queryParamValue1;part'
+  extractPathSegmentFromUrl "${_targetUrl}" 0
+  local -i _rescode=$?
+  local _actualPathSegment="${RESULT}"
+  Assert.isTrue ${_rescode} "extractPathSegmentFromUrl \"${_targetUrl}\" 0 failed"
+  Assert.areEqual "${_expectedPathSegment}" "${_actualPathSegment}" "extractPathSegmentFromUrl \"${_targetUrl}\" 0 returned an invalid value (${_actualPathSegment})"
+  _expectedPathSegment="segment1"
+  extractPathSegmentFromUrl "${_targetUrl}" 1
+  _rescode=$?
+  _actualPathSegment="${RESULT}"
+  Assert.isTrue ${_rescode} "extractPathSegmentFromUrl \"${_targetUrl}\" 1 failed"
+  Assert.areEqual "${_expectedPathSegment}" "${_actualPathSegment}" "extractPathSegmentFromUrl \"${_targetUrl}\" 1 returned an invalid value (${_actualPathSegment})"
 }
 
 function urlIsUp_works_test() {
-  local _url="https://www.google.com";
-  urlIsUp "${_url}";
-  local -i _rescode=$?;
-  Assert.isTrue ${_rescode} "urlIsUp failed for ${_url}";
+  local _url="https://www.google.com"
+  urlIsUp "${_url}"
+  local -i _rescode=$?
+  Assert.isTrue ${_rescode} "urlIsUp failed for ${_url}"
 }
 
-setScriptDescription "Runs all tests implemented for url.dw";
+function extractParameterFromUrl_works_test() {
+  DW.import url
+  local _url="https://www.example.com/segment0/segment1/segment2/lastSegment?queryParamName0=queryParamValue0&queryParamName1=queryParamValue1;part"
+  local _expectedValue="queryParamValue0"
+  extractParameterFromUrl "${_url}" "queryParamName0"
+  local -i _rescode=$?
+  local _actualValue="${RESULT}"
+  Assert.isTrue ${_rescode} "extractParameterFromUrl failed for ${_url}"
+  Assert.areEqual "${_expectedValue}" "${_actualValue}" "extractParameterFromUrl returned an invalid value (${_actualValue})"
+}
+
+setScriptDescription "Runs all tests implemented for url.dw"
 # vim: syntax=sh ts=2 sw=2 sts=4 sr noet
